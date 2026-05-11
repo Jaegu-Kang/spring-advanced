7048e7ab12f6779403c883777810b74c7ad8cb8a
Jaegu-Kang <rkdworn8@gmail.com>
2026-05-11 11:18:36 +0900

    Lv4 테스트 코드 연습 - 1
    잘못된 입력순서 수정

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -15,14 +15,15 @@ class PasswordEncoderTest {
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
-        // given
+        // given: 비밀번호와 이를 암호화한 비빌번호가 주어짐
         String rawPassword = "testPassword";
         String encodedPassword = passwordEncoder.encode(rawPassword);
 
-        // when
-        boolean matches = passwordEncoder.matches(encodedPassword, rawPassword);
+        // when: 암호화된 비밀번호와 원본이 서로 일치 하는지 검증
+        // 잘못된 입력순서 수정
+        boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
 
-        // then
+        // then: 결과가 true여야 함
         assertTrue(matches);
     }
 }
